1884dd994f92a4712801b53550d4c3d7dde4972a
Flatbaby <xxflatbabyxx@gmail.com>
2023-12-25 14:49:23 +0100

    Update hello-world.go

--- hello-world.go
+++ hello-world.go
@@ -1,9 +1,31 @@
 package main
 
-import "fmt"
+type Player struct {
+	name  string
+	hp    int
+	level int
+	Xp    int
+}
+type Monster struct {
+	name string
+	hp   int
+	xp   int
+}
 
-var int b
+func newPlayer(name string) *Player {
+	p := Player{name: name}
+	p.hp = 100
+	p.level = 1
+	p.Xp = 0
+	return &p
+}
+func newMonster(name string) *Monster {
+	m := Monster{name: name}
+	m.hp = 30
+	m.xp = 5
+	return &m
+}
 
 func main() {
-	fmt.Println(b)
+
 }
